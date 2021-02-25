a14cfc7f1a916af8cc8c0719296a8d4211369ab6
Thierry Arnoux <thierry.arnoux@gmx.net>
2021-02-25 11:36:00 +0800

    [FIX] Handle empty recordsets

--- web_translate_dialog/static/src/js/web_translate_dialog.js
+++ web_translate_dialog/static/src/js/web_translate_dialog.js
@@ -111,6 +111,7 @@ var TranslateDialog = Dialog.extend({
     set_maxlength: function(){
         // set maxlength if initial field has size attr
         _.each(this.translatable_fields, function(field_name){
+            if ($('[name='+field_name+']')[0] === undefined) return;
             var size = $('[name='+field_name+']')[0].maxLength;
             if (size > 0){
                 this.$('input.oe_translation_field[name$="'+field_name+'"], textarea.oe_translation_field[name$="'+field_name+'"]').attr('maxlength', size);
